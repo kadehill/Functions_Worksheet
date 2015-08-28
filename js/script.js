af4c820d24d5e83ca69da01c9b5fc3ba6d53ed79
kadehill <khill@fullsail.edu>
2015-08-27 20:05:13 -0400

    Circumference

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Kade Hill
+//08/27/15
+//Functions Worksheet
+//SDI 1508
+
+//Circumference
+
+//Calculate the circumference of a circle.
+
+    //Parameter(s) for function:
+//Radius of the circle
+//  Return: Circumference of the circle
+//Result to print to the console: “The circumference of the circle is X”;
+
+//Variables
+var diameter = 20;
+var pie = 3.14;
+var total = calculatetotal();
+console.log("The circumference of the circle is " + total);
+
+//Function
+function calculatetotal(){
+   return diameter * pie;
+
+
+}
