329721eccbe6dec55a31fd2c2f6ca641663625af
kadehill <khill@fullsail.edu>
2015-08-27 20:07:58 -0400

    Stung!

--- js/script.js
+++ js/script.js
@@ -24,3 +24,14 @@ function calculatetotal(){
 
 
 }
+
+
+///Stung!
+
+//It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
+
+    //Given: Victim’s weight (in pounds)
+    //Parameter(s) for function:
+    //Victim’s weight (in pounds)
+    //Return: Number of Bee stings
+    //Result to print to the console: “It takes X bee stings to kill this animal.
